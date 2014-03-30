40a1223da280d522fda0b89e945d721c4bf6b584
John <kai.wang.john@gmail.com>
2014-03-30 12:19:49 +0800

    finish

--- test_scrapy/spiders/MySpider.py
+++ test_scrapy/spiders/MySpider.py
@@ -1,10 +1,12 @@
-import sys
+# coding=utf-8
 import os
+import time
+import sys
 
 from scrapy.selector import Selector
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.http import Request, HtmlResponse
+from scrapy.http import Request
 from selenium import selenium
 from selenium import webdriver
 
@@ -34,6 +36,8 @@ class MySpider(CrawlSpider):
         self.verification_errors = []
         self.selenium = selenium("localhost", 4444, "*firefox", "http://yue.fm/")
         self.selenium.start(driver=webdriver.Chrome())
+
+        self.count = 0
         # self.driver = webdriver.Chrome()
 
     def __del__(self):
@@ -41,36 +45,36 @@ class MySpider(CrawlSpider):
         print self.verification_errors
         CrawlSpider.__del__(self)
 
-    def parse(self, response):
-        print 'loading url:' + response.url
 
-        # print response.body
-        # driver = self.driver
-        # driver.get(response.url)
-        # next_url = driver.find_element_by_xpath("//div[@class='next']/a[@id='next']").text()
-        # print next_url
-        # yield Request(next_url)
+    def parse(self, response):
+        self.count += 1
+        if self.count >= 10:
+            print '下载了十个，休息五秒'
+            self.count = 0
+            time.sleep(5)
+        print 'loading url:' + response.url
 
         sel = self.selenium
         sel.open(response.url)
         sel.wait_for_page_to_load("2000")
-        import time
 
         time.sleep(1.5)
 
-        response = response.replace(body=sel.get_eval(\
-            "selenium.browserbot.getCurrentWindow().document.getElementsByTagName('html')[0].innerHTML"))
+        body_reg = "selenium.browserbot.getCurrentWindow().document.getElementsByTagName('html')[0].innerHTML"
+        response = response.replace(body=sel.get_eval(body_reg).encode('utf-8'))
 
         s = Selector(response)
 
         title = s.xpath("//div[@class='article']/h1[@class='title'][1]/text()").extract()[0]
-        author = s.xpath("//div[@class='article']/div[@class='body']/p[1]/b/text()").extract()[0]
+        author = s.xpath("//div[@class='article']/div[@class='body']/p[1]/b/text()").extract()
+        if len(author) != 0:
+            author = author[0]
         content = s.xpath("//div[@class='article']/div[@class='body']/p[position()>1]").extract()
 
         self.write_content(title, content)
 
         next_url = "http://yue.fm" + s.xpath("//div[@class='next']/a[@id='next']/@href").extract()[0]
-        print next_url
+        print "next_url:" + next_url
         yield Request(next_url)
 
         item = TestScrapyItem()
@@ -80,7 +84,6 @@ class MySpider(CrawlSpider):
         item['content'] = content
 
         yield item
-
 
     def write_content(self, title, content):
         filename = self.path + title
